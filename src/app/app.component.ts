d801cb72ed65d35d8e8e366d1882aeaac814b04d
voldo <filipecavoldo@outlook.com>
2024-03-05 19:20:27 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArtsTableComponent } from './arts-table/arts-table.component';
+import { HeaderComponent } from './header/header.component';
+import { TopmenuComponent } from './topmenu/topmenu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+      RouterOutlet, 
+      ArtsTableComponent,
+      HeaderComponent,
+      TopmenuComponent
+    ],
+  template: `
+    <app-header></app-header>
+    <div id="content">
+      <app-topmenu></app-topmenu>
+      <app-arts-table></app-arts-table>
+    </div>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HyoutaSymphonia';
+
 }
