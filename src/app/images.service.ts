666ed6a301c44bd73ad12490cbf1f569cda391cb
voldo <filipecavoldo@outlook.com>
2024-03-07 14:51:05 -0300

    added path to new images

--- src/app/images.service.ts
+++ src/app/images.service.ts
@@ -9,13 +9,23 @@ export class ImagesService {
 
   getImagePath(imageAlias: string): string {
     const iconClassMap: { [key: string] : string } = {
-      "lvl1": "./assets/artes-00.png",
-      "lvl2": "./assets/artes-01.png",
-      "slvl1": "./assets/artes-00-spell.png",
+      "lvl1": "./assets/lvl1.webp",
+      "lvl2": "./assets/lvl2.webp",
+      "lvl3": "./assets/lvl3.webp",
+      "slvl1": "./assets/slvl1.webp",
+      "slvl2": "./assets/slvl2.webp",
+      "slvl3": "./assets/slvl3.webp",
       "ROI" : "./assets/ROI.png",
       "COR": "./assets/COR.png",
       "GEN": "./assets/GEN.png",
-      "fire": "./assets/icon-element-02.png"
+      "dark": "./assets/iconDark.webp",
+      "earth": "./assets/iconEarth.webp",
+      "fire": "./assets/iconFire.webp",
+      "ice": "./assets/iconIce.webp",
+      "thunder": "./assets/iconThunder.webp",
+      "water": "./assets/iconWater.webp",
+      "wind": "./assets/iconWind.webp",
+      "angel": "./assets/sAngel.webp"
     }
     return iconClassMap[imageAlias] || '';
   }
