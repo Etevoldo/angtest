5cf225358c0a3014b6ce7f29d39d8ad9201a536e
voldo <filipecavoldo@outlook.com>
2024-03-15 18:38:01 -0300

    added other character icons

--- src/app/topmenu/topmenu.component.ts
+++ src/app/topmenu/topmenu.component.ts
@@ -14,6 +14,18 @@ import { ButtonService } from '../button.service';
       title="Show Collete Only" >
     <input type="image" [src]="getImagePath('GEN')" (click)="toggle(2)"
       title="Show Genis Only" >
+    <input type="image" [src]="getImagePath('REF')" (click)="toggle(2)"
+      title="Show Reine Only" >
+    <input type="image" [src]="getImagePath('KRA')" (click)="toggle(2)"
+      title="Show Krato Only" >
+    <input type="image" [src]="getImagePath('SHI')" (click)="toggle(2)"
+      title="Show Sheena Only" >
+    <input type="image" [src]="getImagePath('ZER')" (click)="toggle(2)"
+      title="Show Zelos Only" >
+    <input type="image" [src]="getImagePath('PRE')" (click)="toggle(2)"
+      title="Show Presea Only" >
+    <input type="image" [src]="getImagePath('REG')" (click)="toggle(2)"
+      title="Show Regal Only" >
     <input type="image" [src]="getImagePath('fire')" (click)="showall()" 
       title="Show All" >
   `,
